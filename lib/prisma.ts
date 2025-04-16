1b2235acb48b30941bb1dc4fa98ecc9bda1a59ad
example <email@example.com>
2025-04-16 13:53:38 +0500

    solving fetch problem in server

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -13,23 +13,40 @@
 
 // export default prisma;
 
-// Make sure your Prisma client handles production environment properly
+
+
+
+// // lib/prisma.ts
+// import { PrismaClient } from '@prisma/client';
+
+// // Define an interface for the custom global with prisma
+// interface CustomNodeJsGlobal {
+//   prisma: PrismaClient | undefined;
+// }
+
+// // Declare the global this way to avoid namespace
+// declare const global: CustomNodeJsGlobal;
+
+// // Prevent multiple instances of Prisma Client in development
+// const prisma = global.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV === 'development') {
+//   global.prisma = prisma;
+// }
+
+// export default prisma;
+
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-// Define an interface for the custom global with prisma
-interface CustomNodeJsGlobal {
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
-}
+};
 
-// Declare the global this way to avoid namespace
-declare const global: CustomNodeJsGlobal;
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-// Prevent multiple instances of Prisma Client in development
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === 'development') {
-  global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
